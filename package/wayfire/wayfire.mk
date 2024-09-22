4fc8e13786c8040893bf4c49830c9060ec9f07e2
Jerome Audu <jau@free.fr>
2024-09-22 16:43:20 +0200

    package/wayfire: new package
    Wayfire is a 3D Wayland compositor, inspired by Compiz and
    based on wlroots. It aims to create a customizable, extendable
    and lightweight environment without sacrificing its appearance.

---
+++ package/wayfire/wayfire.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# wayfire
+#
+################################################################################
+
+WAYFIRE_VERSION = 158ff3e17302397ec8e54728e006469627096777
+WAYFIRE_SITE = https://github.com/WayfireWM/wayfire
+WAYFIRE_SITE_METHOD = git
+WAYFIRE_GIT_SUBMODULES = YES
+WAYFIRE_LICENSE = MIT
+WAYFIRE_LICENSE_FILES = LICENSE
+WAYFIRE_DEPENDENCIES = host-pkgconf wlroots glm json-for-modern-cpp cairo pango
+WAYFIRE_INSTALL_STAGING = YES
+WAYFIRE_CONF_OPTS = \
+	-Dwerror=false
+
+define WAYFIRE_USERS
+	wayfire -1 wayfire -1 * /home/wayfire /bin/sh seat,audio,video Wayfire user
+endef
+
+$(eval $(meson-package))
